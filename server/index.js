a4600225dbdf35e4d5f869eba71f82f62a6e6725
Fl4m3Ph03n1x <palmelapedro@gmail.com>
2017-02-17 16:30:01 +0000

    Laid down general algorithm. Minor fixes

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-// "use strict";
+"use strict";
 
 let jsonfile = require("jsonfile");
 let Promise = require("promise");
@@ -8,7 +8,6 @@ let chai = require("chai"),
 let _ = require("underscore");
 let HTTPStatus = require('http-status');
 let superagent = require("superagent");
-let values = require("object.values");
 
 let ModScraper = require("./promisedScraper.js");
 let exception = require("./Exception.js");
@@ -23,26 +22,13 @@ let scrapperCfg = jsonfile.readFileSync(CONFIG_FILES.SCRAPPER);
 let scrapy = new ModScraper(scrapperCfg);
 
 
-// let getURLsList = function(modsList) {
-//     let urlList = [];
-
-//     for (let mod of modsList)
-//         urlList.push(mod.NameLink);
-
-//     return urlList;
-// };
-
-// let makeBatchRequest = function(linksList) {
-//     let promisesArray = [];
-
-//     let wikiaURL = URL(scrapy.config.sources.wikia.link, true);
-
-//     for (let link of linksList) {
-//         promisesArray.push(scrapy.getModInformation(wikiaURL.protocol + "//" + wikiaURL.hostname + link));
-//     }
-
-//     return new Promise.all(promisesArray);
-// };
+let createLog = function(info) {
+    errorParams.exceptionName = info.exceptionName;
+    exception(errorParams).write({
+        exception: info.exception,
+        item: info.item
+    });
+};
 
 //Does the item conform to our DB schema?
 let checkValidity = function(item) {
@@ -89,59 +75,84 @@ let checkValidity = function(item) {
         expect(item).to.have.property("Category");
         expect(item.Category).to.be.a("string");
         expect(TYPE.CATEGORIES).to.include(item.Category);
+
+        return true;
     }
     catch (error) {
-        errorParams.exceptionName = "ValidityException";
-        exception(errorParams).write(error);
+        createLog({
+            exceptionName: "ValidityException",
+            exception: error,
+            item
+        });
     }
+
+    return false;
 };
 
+//Do this values depict reality? AKA, are the links and other info existent?
+/**
+ *  Return true or false once all the requests are complete. PROMISIFY!
+ */
 let checkAccuracy = function(item) {
     const wikiaURL = new URL(scrapperCfg.sources.wikia.link);
 
-    try {
+    superagent.get(wikiaURL.origin + item.NameLink, (error, res) => {
+        try {
+            expect(error).to.not.exist;
+            expect(res.status).to.equal(HTTPStatus.OK);
+        }
+        catch (error) {
+            createLog({
+                exceptionName: "AccuracyException",
+                exception: error,
+                item
+            });
+        }
 
-        superagent.get(item.NameLink, (error, res) => {
+    });
 
+    superagent.get(item.PolarityLink, (error, res) => {
+
+        try {
+            expect(error).to.not.exist;
+            expect(res.status).to.equal(HTTPStatus.OK);
+        }
+        catch (error) {
+            createLog({
+                exceptionName: "AccuracyException",
+                exception: error,
+                item
+            });
+        }
+    });
+
+    if (!_.isUndefined(item.CategoryLink)) {
+        superagent.get(wikiaURL.origin + item.CategoryLink, (error, res) => {
             try {
                 expect(error).to.not.exist;
                 expect(res.status).to.equal(HTTPStatus.OK);
             }
             catch (error) {
-                errorParams.exceptionName = "AccuracyException";
-                exception(errorParams).write({
+                createLog({
+                    exceptionName: "AccuracyException",
                     exception: error,
                     item
                 });
             }
         });
-
-        superagent.get(wikiaURL.origin + item.NameLink, (error, res) => {
-            expect(error).to.not.exist;
-            expect(res.status).to.equal(HTTPStatus.OK);
-        });
-
-        superagent.get(item.PolarityLink, (error, res) => {
-            expect(error).to.not.exist;
-            expect(res.status).to.equal(HTTPStatus.OK);
-        });
-
-        if (!_.isUndefined(item.CategoryLink)) {
-            superagent.get(wikiaURL.origin + item.CategoryLink, (error, res) => {
-                expect(error).to.not.exist;
-                expect(res.status).to.equal(HTTPStatus.OK);
-            });
-        }
-    }
-    catch (error) {
-        errorParams.exceptionName = "AccuracyException";
-        exception(errorParams).write(error);
     }
 };
 
 //Do different sources report the same informaiton about the same item?
+/**
+ *  Return true or false once all the requests are complete. PROMISIFY!
+ */
 let checkConsistency = function(item) {
-
+    /**
+     * 1. Make request to item URL
+     * 2. Scrap data from that page
+     * 3. Compare data from page with item data
+     */
 };
 
 let checkData = function(mods) {
@@ -149,62 +160,19 @@ let checkData = function(mods) {
     for (let item of mods) {
         checkValidity(item);
         checkAccuracy(item);
-        // expect(item).to.have.property("NameLink");
-        // expect(item.NameLink).to.be.a("string");
-        // expect(item.NameLink).to.not.be.empty;
+        checkConsistency(item);
 
-        // superagent.get(wikiaUri + item.NameLink, (error, res) => {
-        //     expect(error).to.not.exist;
-        //     expect(res.status).to.equal(HTTPStatus.OK);
-        // });
-
-        // expect(item).to.have.property("Name");
-        // expect(item.Name).to.be.a("string");
-        // expect(item.Name).to.not.be.empty;
-
-        // expect(item).to.have.property("Description");
-        // expect(item.Description).to.be.a("string");
-        // expect(item.Description).to.not.be.empty;
-        // expect(item.Description[item.Description.length - 1]).to.equal("."); //Each description must terminate with correct puntuation. I am freak rigth? xD
-
-        // expect(item).to.have.property("PvPOnly");
-        // expect(item.PvPOnly).to.be.a("boolean");
-
-        // expect(item).to.have.property("Polarity");
-        // expect(item.Polarity).to.be.a("string");
-        // expect(TYPE.POLARITIES).to.include(item.Polarity);
-
-        // expect(item).to.have.property("PolarityLink");
-        // expect(item.PolarityLink).to.be.a("string");
-        // expect(item.PolarityLink).to.not.be.empty;
-        // superagent.get(item.PolarityLink, (error, res) => {
-        //     expect(error).to.not.exist;
-        //     expect(res.status).to.equal(HTTPStatus.OK);
-        // });
-
-        // expect(item).to.have.property("Rarity");
-        // expect(item.Rarity).to.be.a("string");
-        // expect(TYPE.RARITIES).to.include(item.Rarity);
-
-        // if (item.CategoryLink) {
-        //     expect(item.CategoryLink).to.be.a("string");
-        //     expect(item.CategoryLink).to.not.be.empty;
-        //     superagent.get(wikiaUri + item.CategoryLink, (error, res) => {
-        //         expect(error).to.not.exist;
-        //         expect(res.status).to.equal(HTTPStatus.OK);
-        //     });
-        // }
-
-        // expect(item).to.have.property("Category");
-        // expect(item.Category).to.be.a("string");
-        // expect(TYPE.CATEGORIES).to.include(item.Category);
+        /**
+         *  1. Do I have this in the DB?
+         *  1.1 No? Save it with timestamp.
+         *  1.2 Yes? Compare to DB version and save if different. Update timestamp.
+         */
     }
 };
 
 //Get General information from mod tables
 scrapy.getWarframeMods()
     .then(checkData)
-    .then(() => console.log("Data check complete, saving to DB"))
     .catch(console.log);
 
 
